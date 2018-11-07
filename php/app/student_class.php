5b636af4896a650217aaca7ccfdc5c29401836eb
eugeo <tlknvr@gmail.com>
2018-11-07 21:49:56 +0800

    student_class changed

--- php/app/student_class.php
+++ php/app/student_class.php
@@ -12,16 +12,38 @@ if(!empty($_POST['phoneNumber'])){
     $phone = $_POST['phoneNumber'];
     $sql = "SELECT `id` AS total FROM `zbjy_Student` WHERE `phoneNumber` = '{$phone}'";
     $obj = mysqli_query($con,$sql);
+    $res = mysqli_fetch_assoc($obj);
         //获取课程变量将其插入数据库
     $id = $res['total'];
-    $classTime = $_POST['class_time'];
-    $url = $_POST['class_url'].$_SESSION['num'];
+    $teacher_id = $_POST['teacher_id'];
     $subject = $_POST['subject'];
-    $classWeek = $_POST['class_week'];
-    $sql = "INSERT `zbjy_student_subject`(`user_id`,`create_time`,`subject`,`class_time`,`class_url`,`class_week`) values('{$id}','{$_SERVER['REQUEST_TIME']}','{$subject}','{$classTime}','{$url}','{$classWeek}')";
+    $sql = "select * from `teacher_subject` where `teacher_id` = '{$teacher_id}' and `subject` = '{$subject}'";
     $obj = mysqli_query($con,$sql);
     if($obj){
-        echo '插入成功';
+        //数组计数器
+        $i = 0;
+        $val = '';
+        unset($sql);
+        while($result = mysqli_fetch_assoc($obj)){
+            $res[$i] = $result;
+            $classTime = $res[$i]['class_time'];
+            $url = $res[$i]['class_url'];
+            $classWeek = $res[$i]['class_week'];
+            $intro = $res[$i]['intro'];
+            $suit_people = $res[$i]['suit_people'];
+            $img_url = $res[$i]['imt_url'];
+            $updated = $res[$i]['section_number'];
+            $i++;
+            $val .= "('".{$updated}."',".{$img_url}.",".{$teacher_id}.",".{$suit_people}.",".{$intro}.",".{$user_id}.",".{$_SERVER['REQUEST_TIME']}.",".{$subject}.",".{$classTime}.",".{$url}.",".{$classWeek}.",)";
+        }
+        $val = rtrim($val,',');
+        $sql = "INSERT `zbjy_student_subject`(`updated`,`img_url`,`teacher_id`,`suit_people`,`intro`,`user_id`,`create_time`,`subject`,`class_time`,`class_url`,`class_week`) values $val";
+        $obj = mysqli_query($con,$sql);
+        if($obj){
+            echo '插入成功';
+        }else {
+            echo mysqli_error($con);
+        }
     }else {
         echo mysqli_error($con);
     }
